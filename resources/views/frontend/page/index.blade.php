029bc3f947078a06e0f8c5d5e56169958924673c
Ahmad Badrul Ulum <badrul@lenna.ai>
2021-07-21 03:32:23 +0700

    add js custom

--- resources/views/frontend/page/index.blade.php
+++ resources/views/frontend/page/index.blade.php
@@ -1,5 +1,42 @@
 @extends('frontend.layouts.base')
 
+@section('add-js')
+<script type="text/javascript">
+    function active_course() {
+        if ($(".active_course").length) {
+            $(".active_course").owlCarousel({
+                loop: true,
+                margin: 20,
+                items: 3,
+                nav: true,
+                autoplay: 2500,
+                smartSpeed: 1500,
+                dots: false,
+                responsiveClass: true,
+                thumbs: true,
+                thumbsPrerendered: true,
+                navText: ["<img src='https://colorlib.com/preview/theme/edustage/img/prev.png'>", "<img src='https://colorlib.com/preview/theme/edustage/img/next.png'>"],
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    991: {
+                        items: 2,
+                        margin: 30
+                    },
+                    1200: {
+                        items: 3,
+                        margin: 30
+                    }
+                }
+            });
+        }
+    }
+    active_course(); 
+</script>
+@endsection
+
 @section('content')
 <section id="home" class="intro intro-bg parallax">
     <div class="container mt-5">
