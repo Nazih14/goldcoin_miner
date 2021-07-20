8f79cc9f486f3d214442f1072e8db4fe1f8ce318
Ahmad Badrul Ulum <badrul@lenna.ai>
2021-07-21 02:52:57 +0700

    add asset frontend

---
+++ public/media/js/custom.js
@@ -0,0 +1,31 @@
+
+var wow = new WOW(
+{
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+  );
+wow.init();
+
+ // Sticky Header
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        $(".top-nav").addClass("light-header");
+    } else {
+        $(".top-nav").removeClass("light-header");
+    }
+});
+
+/*------ slide menu collapse ------*/
+     
