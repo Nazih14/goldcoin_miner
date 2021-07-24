20588c784988821c40bd9f7bb2b144a32f98d9b7
Ahmad Badrul Ulum <badrul@lenna.ai>
2021-07-25 06:38:51 +0700

    update login page new template

---
+++ resources/views/auth/login-backup.blade.php
@@ -0,0 +1,80 @@
+@extends('frontend.layouts.base')
+
+@section('content')
+
+<br><br>
+<div class="container h-100 mt-5 mb-5">
+    <div class="row h-100">
+        <div class="col-sm-10 col-md-8 col-lg-4 mx-auto d-table h-100">
+            <div class="d-table-cell align-middle">
+
+                <div class="text-center mt-4">
+                    <h1 class="h2">Welcome back, Dey</h1>
+                    <p class="lead">
+                        Sign in to your account to continue
+                    </p>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <div class="m-sm-4">
+                            <div class="text-center">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Andrew Jones" class="img-fluid rounded-circle" width="112" height="112">
+                            </div>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input id="email" type="email" class="form-control  form-control-md @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus  placeholder="Enter your email">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input id="password" type="password" class="form-control form-control-m @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter your password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    {{-- <small>
+                                        <a href="pages-reset-password.html">Forgot password?</a>
+                                    </small> --}}
+                                </div>
+                                <div>
+                                    <div class="custom-control custom-checkbox align-items-center">
+                                        <input type="checkbox" class="custom-control-input" value="remember" name="remember"  id="remember" checked="">
+                                        {{-- <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> --}}
+                                        <label class="custom-control-label text-small">Remember me next time</label>
+                                    </div>
+                                </div>
+                                <div class="text-center mt-3">
+                                    <button type="submit" class="btn btn-primary btn-md">
+                                        {{ __('Login') }}
+                                    </button>
+                                </div>
+                            </form>
+
+                            {{-- @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif --}}
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <center>
+                    <a href="/"><p><i class="fas fa-home"></i>  Beranda</p></a>
+                </center>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
