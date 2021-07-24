ac656e7aa3443b35eb559ece644386b46c8fb566
Ahmad Badrul Ulum <badrul@lenna.ai>
2021-07-25 02:21:43 +0700

    form update profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 use App\Http\Controllers\Frontend\HomePageController;
-use App\Http\Controllers\Admin\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,14 +24,16 @@ Route::get('/faq', [HomePageController::class, 'faq']);
 
 // admin area
 Route::middleware(['admin', 'auth'])->prefix('admin')->group(function(){
-    Route::get('transactions', [TransactionController::class, 'index'])->name('admin.transactions');;
+    Route::get('transactions', [App\Http\Controllers\Admin\TransactionController::class, 'index'])->name('admin.transactions');;
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('profile', [App\Http\Controllers\Admin\UserController::class, 'profile'])->name('admin.profile');
 });
 
 
 // member area
 Route::middleware(['auth'])->prefix('member')->group(function(){
     Route::get('dashboard', [App\Http\Controllers\Member\DashboardController::class, 'index'])->name('member.dashboard');
+    Route::get('profile', [App\Http\Controllers\Admin\UserController::class, 'profile'])->name('member.profile');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
